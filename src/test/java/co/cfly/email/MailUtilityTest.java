f8d9ece8fa065dc4bc1b9a3aea7753be4b185aaa
Cody Lerum <cody.lerum@gmail.com>
2025-06-10 13:07:01 -0600

    use first

--- src/test/java/co/cfly/email/MailUtilityTest.java
+++ src/test/java/co/cfly/email/MailUtilityTest.java
@@ -56,7 +56,7 @@ public class MailUtilityTest {
             MimeMessage mimeMessage = new MimeMessage(Session.getDefaultInstance(new Properties()), inputStream);
             EmailMessage emailMessage = MessageConverter.convert(mimeMessage);
             Assert.assertEquals(1, emailMessage.getAttachments().size());
-            Assert.assertEquals("test.csv", emailMessage.getAttachments().get(0).getFileName());
+            Assert.assertEquals("test.csv", emailMessage.getAttachments().getFirst().getFileName());
             Assert.assertTrue(emailMessage.getTextBody().startsWith("Ticket T29345"));
             Assert.assertTrue(emailMessage.getHtmlBody().startsWith("<!DOCTYPE html PUBLIC"));
             Assert.assertEquals("Ticket T29345 - New Comment", emailMessage.getSubject());
@@ -69,8 +69,8 @@ public class MailUtilityTest {
             MimeMessage mimeMessage = new MimeMessage(Session.getDefaultInstance(new Properties()), inputStream);
             EmailMessage emailMessage = MessageConverter.convert(mimeMessage);
             Assert.assertEquals(1, emailMessage.getAttachments().size());
-            Assert.assertEquals("Messages.pdf", emailMessage.getAttachments().get(0).getFileName());
-            Assert.assertEquals(ContentDisposition.ATTACHMENT, emailMessage.getAttachments().get(0).getContentDisposition());
+            Assert.assertEquals("Messages.pdf", emailMessage.getAttachments().getFirst().getFileName());
+            Assert.assertEquals(ContentDisposition.ATTACHMENT, emailMessage.getAttachments().getFirst().getContentDisposition());
             Assert.assertNull(emailMessage.getTextBody());
             Assert.assertNull(emailMessage.getHtmlBody());
             Assert.assertEquals("Test Subject", emailMessage.getSubject());
@@ -83,8 +83,8 @@ public class MailUtilityTest {
             MimeMessage mimeMessage = new MimeMessage(Session.getDefaultInstance(new Properties()), inputStream);
             EmailMessage emailMessage = MessageConverter.convert(mimeMessage);
             Assert.assertEquals(1, emailMessage.getAttachments().size());
-            Assert.assertEquals("05062021scan05062021.pdf", emailMessage.getAttachments().get(0).getFileName());
-            Assert.assertEquals(ContentDisposition.ATTACHMENT, emailMessage.getAttachments().get(0).getContentDisposition());
+            Assert.assertEquals("05062021scan05062021.pdf", emailMessage.getAttachments().getFirst().getFileName());
+            Assert.assertEquals(ContentDisposition.ATTACHMENT, emailMessage.getAttachments().getFirst().getContentDisposition());
             Assert.assertTrue(emailMessage.getTextBody().contains("HIPAA CONFIDENTIALITY NOTICE"));
             Assert.assertNull(emailMessage.getHtmlBody());
             Assert.assertEquals("SWC Scanned", emailMessage.getSubject());
